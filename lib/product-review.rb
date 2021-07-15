8b635b1ed0774c08aca605382b61263615389df2
Forrest Reiland <you@example.com>
2021-07-14 17:12:22 -0700

    add sort method, manually test, working

--- lib/product-review.rb
+++ lib/product-review.rb
@@ -10,8 +10,37 @@ class Product
     end
   end 
 
-  def sort()
-    
+  def sort_items()
+    approve=[]
+    reject=[]
+    File.open(@file_name).each do |line|
+      if line.match(/reject/)
+        item = line.scan(/(?<=\,)(.*?)(?=\- )/)
+        reject.push(item)
+      else 
+        item = line.scan(/(?<=\,)(.*?)(?=\- )/)
+        approve.push(item)
+
+      end
+    end 
+      flat_approve = approve.flatten.uniq.sort
+      flat_reject = reject.flatten.uniq.sort
+      print 'REJECT'
+      print "\n"
+      flat_reject.each do |e|
+        print e
+        print "\n"
+        
+      end
+      
+      new_array=flat_approve-flat_reject
+      print 'APPROVE'
+      print "\n"
+      new_array.each do |e|
+        print e 
+        print "\n"
+      end 
   end
+
 end
 
