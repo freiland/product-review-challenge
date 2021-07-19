dce21b4bfb1b15d2657b015e83e93b261e49b83f
Forrest Reiland <you@example.com>
2021-07-19 16:12:06 -0700

    write test for removing duplicates from approve array, test pass

--- lib/product-review.rb
+++ lib/product-review.rb
@@ -27,31 +27,32 @@ class Product
   def sort_reject()
     reject=[]
     File.open(@file_name).each do |line|
+      if line.match(/reject/)
       item = line.scan(/(?<=\,)(.*?)(?=\- )/)
       reject.push(item)
     end
   end
+  return reject 
+  end
 
 
-  def other_methods()
-      flat_approve = approve.flatten.uniq.sort
-      flat_reject = reject.flatten.uniq.sort
-      print 'REJECT'
-      print "\n"
-      flat_reject.each do |e|
-        print e
-        print "\n"
-        
-      end
+  def remove_approve_duplicates(array)
+     return array.flatten.uniq.sort
+    
+  end
+
+  def remove_reject_duplicates()
+    flat_reject = reject.flatten.uniq.sort
+  end
       
-      new_array=flat_approve-flat_reject
-      print 'APPROVE'
-      print "\n"
-      new_array.each do |e|
-        print e 
-        print "\n"
-      end 
-    end
+      # new_array=flat_approve-flat_reject
+      # print 'APPROVE'
+      # print "\n"
+      # new_array.each do |e|
+      #   print e 
+      #   print "\n"
+      # end 
+    
   
 
 end
--- spec/product_review_spec.rb
+++ spec/product_review_spec.rb
@@ -8,10 +8,22 @@ describe ('product') do
   end
 
   it('pushes all approve items to an array') do 
-
-    
     new_data = Product.new('sample-input.txt')
     expect(new_data.sort_approve().length).to(eq(6))
   end
+
+  it('pushes all reject items to a reject array') do 
+    new_data = Product.new('sample-input.txt')
+    expect(new_data.sort_reject().length).to(eq(3))
+  end
+
+  it('removes duplicates from the approve array') do 
+    new_data=Product.new('sample-input.txt')
+    new_array = new_data.sort_approve()
+    expect(new_data.remove_approve_duplicates(new_array).length).to(eq(4))
+  end
+
+
+
 
 end
